c928dd574b13e169a85e58a474cecc3e80398806
rbadr <rahal.badr@gmail.com>
2017-01-07 23:22:01 +0100

    Load the parameters directly from the config file

--- src/main/java/fr/univlille1/m2iagl/sma/simulation/Main.java
+++ src/main/java/fr/univlille1/m2iagl/sma/simulation/Main.java
@@ -8,8 +8,11 @@ import fr.univlille1.m2iagl.sma.sma.SMA;
 import fr.univlille1.m2iagl.sma.view.MainView;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Properties;
 import javax.swing.JFrame;
  
 public class Main {
@@ -18,38 +21,45 @@ public class Main {
           throw new IllegalAccessError("Main class");
       }
     
-    public static void main(String[] args) throws InterruptedException{
-         int gridSizeX = 100;
-         int gridSizeY = 100;
-         int numberOfBlueParticles = 50;
-         int numberOfGreenParticles = 50;
-         int canvasSizeX=800;
-         int canvasSizeY=600;
-         int delay = 50;
+    public static void main(String[] args) throws InterruptedException, IOException{
+        
+        Properties prop = new Properties();
+        ClassLoader loader = Thread.currentThread().getContextClassLoader();
+        InputStream stream = loader.getResourceAsStream("config.properties");
+        prop.load(stream);
+        
+        int gridSizeX = Integer.parseInt(prop.getProperty("gridSizeX"));
+        int gridSizeY = Integer.parseInt(prop.getProperty("gridSizeY"));
+        int numberOfBlueParticles = Integer.parseInt(prop.getProperty("nbParticles"))/2;
+        int numberOfGreenParticles = Integer.parseInt(prop.getProperty("nbParticles"))/2;
+        int canvasSizeX=Integer.parseInt(prop.getProperty("canvasSizeX"));
+        int canvasSizeY=Integer.parseInt(prop.getProperty("canvasSizeY"));
+        int delay = Integer.parseInt(prop.getProperty("delay"));
  
-         Environment<Agent> environment = new Environment<>(gridSizeX,gridSizeY);
+        Environment<Agent> environment = new Environment<>(gridSizeX,gridSizeY);
  
-         List<Agent> agents = new LinkedList<>();
-         for(int i = 0; i < numberOfBlueParticles; i ++) {
-             agents.add(new BlueParticle(environment));
-         }
-         for(int i = 0; i < numberOfGreenParticles; i++) {
-             agents.add(new RedParticle(environment));
-         }
-         environment.initEnvironment(agents);
+        List<Agent> agents = new LinkedList<>();
+        for(int i = 0; i < numberOfBlueParticles; i ++) {
+            agents.add(new BlueParticle(environment));
+        }
+        for(int i = 0; i < numberOfGreenParticles; i++) {
+            agents.add(new RedParticle(environment));
+        }
+        environment.initEnvironment(agents);
  
-         final SMA<Agent> sma = new SMA<>(environment, delay);
+        final SMA<Agent> sma = new SMA<>(environment, delay);
  
-         JFrame frame = new MainView("Système multi-agents", sma, canvasSizeX, canvasSizeY);
-         frame.setVisible(true);
+        JFrame frame = new MainView("Système multi-agents", sma, canvasSizeX, canvasSizeY);
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
  
-         frame.addWindowListener(new WindowAdapter() {
-             @Override
-             public void windowClosing(WindowEvent e) {
-                 super.windowClosing(e);
-                 sma.endSimulation(true);
-             }
-         });
-         sma.run();
-     }
- }
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                super.windowClosing(e);
+                sma.endSimulation(true);
+            }
+        });
+        sma.run();
+    }
+}
