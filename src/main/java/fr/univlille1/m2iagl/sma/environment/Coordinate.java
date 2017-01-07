858ec8876cecfcf961d23201b854f516fb4f65ee
rbadr <rahal.badr@gmail.com>
2017-01-07 18:14:56 +0100

    Initialize particles coordinates

---
+++ src/main/java/fr/univlille1/m2iagl/sma/environment/Coordinate.java
@@ -0,0 +1,21 @@
+package fr.univlille1.m2iagl.sma.environment;
+
+public class Coordinate {
+
+    private int x;
+    private int y;
+
+    public Coordinate(int x, int y) {
+        super();
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
