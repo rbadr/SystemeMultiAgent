a92b5012c21933c10231fcb4a0bdb5e7167639cd
rbadr <rahal.badr@gmail.com>
2017-01-13 14:52:25 +0100

    optimize coordinate

--- src/main/java/fr/univlille1/m2iagl/sma/environment/Coordinate.java
+++ src/main/java/fr/univlille1/m2iagl/sma/environment/Coordinate.java
@@ -18,4 +18,29 @@ public class Coordinate {
     public int getY() {
         return y;
     }
+    
+    @Override
+    public String toString() {
+        return "Coordinate{x=" + x +", y=" + y +'}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Coordinate that = (Coordinate) o;
+
+        if (x != that.x) return false;
+        if (y != that.y) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = x;
+        result = 31 * result + y;
+        return result;
+    }
 }
