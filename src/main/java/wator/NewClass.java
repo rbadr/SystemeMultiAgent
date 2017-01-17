e7b47b739b11edfe230d4a9d90565c749e27ccaa
rbadr <rahal.badr@gmail.com>
2017-01-18 00:31:53 +0100

    feat(new architecture)

---
+++ src/main/java/wator/NewClass.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wator;
+
+/**
+ *
+ * @author Rahal Badr
+ */
+public class NewClass {
+    
+}
